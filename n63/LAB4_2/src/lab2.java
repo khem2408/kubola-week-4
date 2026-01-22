ff0f76c7ca11995329425cbe57b799cee135a7a0
khem2408 <68160024@go.buu.ac.th>
2026-01-23 00:01:04 +0700

    Update lab2.java

--- n63/LAB4_2/src/lab2.java
+++ n63/LAB4_2/src/lab2.java
@@ -1,4 +1,46 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class lab2 {
-
+	static JTextField text;
+	static JLabel lb;
+	static JButton bt;
+	public static void main(String[] args) {
+		JFrame win = new JFrame();
+		win.setBounds(760, 390, 400, 300);
+		win.setTitle("Hello patsachon suetrong 68160157 n87");
+		
+		JPanel bg = new JPanel();
+		bg.setBackground(new Color(255,255,255));
+		win.add(bg);
+		
+		lb = new JLabel("Please enter your name");
+		bg.add(lb);
+		
+		text = new JTextField(13);
+		bg.add(text);
+		
+		bt = new JButton("send");
+		bg.add(bt);
+		
+		bt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) { 
+				String name = text.getText();	
+				try {
+					lb.setText("Hello "+name);
+					text.setVisible(false);
+					bt.setVisible(false);
+			    } catch (NumberFormatException ex) {
+			    	
+			  }
+			}
+		});
+		win.setVisible(true);
+	}
 }
